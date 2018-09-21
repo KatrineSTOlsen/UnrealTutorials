ef0cdbe510e2c505e9a29863791676ad49b44ca9
Katrine Olsen <KatrinestOlsen@gmail.com>
2018-09-21 14:37:29 +0200

    Upload to online repository

---
+++ Source/Section01/OpenDoor.cpp
@@ -0,0 +1,59 @@
+// Copyright Katrine Olsen 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	AActor* Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+}
+
+void UOpenDoor::CloseDoor()
+{
+	AActor* Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.0f, 0.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
+	else if (!PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		CloseDoor();
+	}
+
+	// poll the triggervolume every frame 
+	//if the actor that opens is in the volume
+
+}
+
